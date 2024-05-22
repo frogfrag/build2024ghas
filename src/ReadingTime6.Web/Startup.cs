554394cc2ddc93a7add5c7411c7fb05e4d4e1a1f
frogfrag <breno.assis@outlook.com>
2024-05-22 22:57:46 +0000

    no PAT here

--- src/ReadingTime6.Web/Startup.cs
+++ src/ReadingTime6.Web/Startup.cs
@@ -11,6 +11,7 @@ namespace ReadingTime6.Web
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            var pat = "";
         }
 
         public IConfiguration Configuration { get; }
